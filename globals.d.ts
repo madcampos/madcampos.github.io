eb6e530feaab2a2d947193a5e104c905cdafd41a
madcampos <madcampos@outlook.com>
2023-05-03 22:40:56 -0400

    feat: update project configuration

--- globals.d.ts
+++ globals.d.ts
@@ -24,44 +24,7 @@ interface ImportMetaEnv {
 	readonly DEV: boolean,
 
 	/** The project's base url. */
-	readonly BASE_URL: string,
-	/** The project's public url. */
-	readonly APP_PUBLIC_URL: string,
-
-	/** The app's full name. */
-	readonly APP_NAME: string,
-	/** The app's short name, used for PWAs. */
-	readonly APP_SHORT_NAME: string,
-	/** The app's description. */
-	readonly APP_DESCRIPTION: string,
-	/** The app's keywords. */
-	readonly APP_KEYWORDS: string,
-	/** The app's author. */
-	readonly APP_AUTHOR: string,
-	/** The app's version */
-	readonly APP_VERSION: string,
-
-	/** The app's theme color. */
-	readonly APP_THEME_COLOR: string,
-	/** The app's background color */
-	readonly APP_BACKGROUND_COLOR: string,
-
-	/** The icon used for Apple devices. */
-	readonly APP_APPLE_ICON: string,
-	/** The _small_ icon used for all other devices. */
-	readonly APP_SMALL_ICON: string,
-	/** The _small_ icon used for all other devices, with maskable background. */
-	readonly APP_SMALL_ICON_BG: string,
-	/** The _large_ icon used for all other devices. */
-	readonly APP_LARGE_ICON: string,
-	/** The _large_ icon used for all other devices, with maskable background. */
-	readonly APP_LARGE_ICON_BG: string,
-
-	/** The app's main social image */
-	readonly APP_SOCIAL_IMAGE: string,
-
-	/** The app's database version. */
-	readonly APP_DB_VERSION: string
+	readonly BASE_URL: string
 }
 
 interface ImportMeta {
