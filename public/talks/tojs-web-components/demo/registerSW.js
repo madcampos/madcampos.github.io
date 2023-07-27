2f7a54883bfb5435b5168079f7654253c47b463f
madcampos <madcampos@outlook.com>
2023-07-27 04:41:08 -0400

    feat: move demo to it's own page

---
+++ public/talks/tojs-web-components/demo/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('https://madcampos.dev/talks/tojs-web-components/demo/sw.js', { scope: 'https://madcampos.dev/talks/tojs-web-components/demo/' })})}
