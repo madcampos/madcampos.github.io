c953331a9a12132fca63403f5763f4ce60e6f8c3
madcampos <madcampos@outlook.com>
2024-01-18 23:45:18 -0500

    feat: add related posts to post schema

--- src/schemas/blog.ts
+++ src/schemas/blog.ts
@@ -3,13 +3,15 @@ import { type SchemaContext, z as zod } from 'astro:content';
 export const blogSchema = ({ image }: SchemaContext) => zod.object({
 	title: zod.string().describe('The post title that will be displayed as the top of the post page or as the heading on lists.'),
 	summary: zod.string().describe('A summary for the post that will show in list pages.'),
-	createdAt: zod.date().describe('The post creation date.'),
-	updatedAt: zod.date().optional().describe('The post last update date.'),
+	createdAt: zod.date().describe('The post\'s creation date.'),
+	updatedAt: zod.date().optional().describe('The post\'s last update date.'),
 	draft: zod.boolean().optional().describe('Whether the post is a draft or not.'),
 
-	image: image().optional().describe('The post main "hero" image path, relative to the blog root.'),
-	imageAlt: zod.string().optional().describe('The post main image alt text.'),
+	image: image().optional().describe('Post\'s Main "hero" image path, relative path to the post file.'),
+	imageAlt: zod.string().optional().describe('Main image alt text.'),
 
-	tags: zod.array(zod.string()).optional().describe('The post tags.'),
-	updates: zod.array(zod.string()).optional().describe('The post updates.')
+	tags: zod.array(zod.string()).optional().describe('Tags for this post.'),
+	relatedPosts: zod.array(zod.string()).optional().describe('Slugs for posts related to this.'),
+
+	updates: zod.array(zod.string()).optional().describe('A list of updates this post had.')
 });
--- src/utils/post.ts
+++ src/utils/post.ts
@@ -2,13 +2,19 @@ import { type CollectionEntry, getCollection } from 'astro:content';
 
 export const MAX_POSTS_PER_PAGE = 10;
 
-export type Post = Omit<CollectionEntry<'blog'>, 'slug'> & {
+export interface RelatedPost {
+	slug: string,
+	url: string
+}
+
+export interface Post extends Omit<CollectionEntry<'blog'>, 'slug' | 'relatedPosts'> {
 	slug: string,
 	year: string,
 	month: string,
 	day: string,
-	url: string
-};
+	url: string,
+	relatedPosts: RelatedPost[]
+}
 
 function sortPostsByDate(first: Post, second: Post) {
 	const firstDate = new Date(first.data.updatedAt ?? first.data.createdAt);
@@ -17,9 +23,8 @@ function sortPostsByDate(first: Post, second: Post) {
 	return secondDate.getTime() - firstDate.getTime();
 }
 
-function formatPostMetadata(post: CollectionEntry<'blog'>) {
-	const slug = post.slug.split('/').pop() ?? '';
-	const postDate = new Date(post.data.updatedAt ?? post.data.createdAt);
+function getPostDate(post: CollectionEntry<'blog'>) {
+	const postDate = new Date(post.data.createdAt);
 	const year = postDate.getFullYear().toString();
 	// eslint-disable-next-line @typescript-eslint/no-magic-numbers
 	const month = (postDate.getMonth() + 1).toString().padStart(2, '0');
@@ -27,25 +32,70 @@ function formatPostMetadata(post: CollectionEntry<'blog'>) {
 	const day = postDate.getDate().toString().padStart(2, '0');
 
 	return {
+		date: postDate,
+		year,
+		month,
+		day
+	};
+}
+
+function formatPostSlug(post: CollectionEntry<'blog'>) {
+	const slug = post.slug.split('/').pop() ?? '';
+
+	return slug;
+}
+
+function getPostUrl(post: CollectionEntry<'blog'>) {
+	const slug = formatPostSlug(post);
+	const { year, month } = getPostDate(post);
+
+	return `${year}/${month}/${slug}`;
+}
+
+async function getRelatedPosts(post: CollectionEntry<'blog'>) {
+	const blogEntries = await getCollection('blog');
+	const posts = blogEntries.filter(({ data: { draft } }) => !draft);
+
+	const relatedPosts: RelatedPost[] = [];
+
+	for (const otherPost of posts) {
+		if (post.data.relatedPosts?.includes(otherPost.slug)) {
+			relatedPosts.push({
+				slug: formatPostSlug(otherPost),
+				url: getPostUrl(otherPost)
+			});
+		}
+	}
+
+	return relatedPosts;
+}
+
+async function formatPostMetadata(post: CollectionEntry<'blog'>) {
+	const slug = formatPostSlug(post);
+	const { year, month, day } = getPostDate(post);
+
+	return {
 		...post,
 		slug,
 		year,
 		month,
 		day,
-		url: `${year}/${month}/${slug}`
+		url: getPostUrl(post),
+		relatedPosts: await getRelatedPosts(post)
 	};
 }
 
 export async function listAllPosts() {
 	const blogEntries = await getCollection('blog');
-	const posts = blogEntries.filter(({ data: { draft } }) => !draft).map((entry) => formatPostMetadata(entry)).sort(sortPostsByDate);
+	const filteredBlogEntries = blogEntries.filter(({ data: { draft } }) => !draft);
+	const formattedBlogEntries = await Promise.all(filteredBlogEntries.map(async (entry) => formatPostMetadata(entry)));
+	const sortedBlogEntries = formattedBlogEntries.sort(sortPostsByDate);
 
-	return posts;
+	return sortedBlogEntries;
 }
 
 export async function listPostPagesByYear() {
-	const blogEntries = await getCollection('blog');
-	const posts = blogEntries.filter(({ data: { draft } }) => !draft).map((entry) => formatPostMetadata(entry));
+	const posts = await listAllPosts();
 
 	const years: Record<string, Post[]> = {};
 
@@ -94,8 +144,7 @@ export async function listPostsPagesByMonth() {
 }
 
 export async function listPostPagesByTag() {
-	const blogEntries = await getCollection('blog');
-	const posts = blogEntries.filter(({ data: { draft } }) => !draft).map((entry) => formatPostMetadata(entry));
+	const posts = await listAllPosts();
 
 	const tags: Record<string, Post[]> = {};
 
