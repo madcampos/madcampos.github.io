c349bf55d15b96ce73f27ad0e3ca60f39be07c74
madcampos <madcampos@outlook.com>
2024-01-24 13:04:52 -0500

    feat: update feed xml to include media rss

--- src/pages/blog/feed.xml.ts
+++ src/pages/blog/feed.xml.ts
@@ -1,74 +1,89 @@
 import type { APIRoute } from 'astro';
 
 import { stat } from 'node:fs/promises';
+
 import rss, { type RSSFeedItem } from '@astrojs/rss';
-import { listAllPosts } from '../../utils/post';
 import { getImage } from 'astro:assets';
+
+import { listAllPosts } from '../../utils/post';
 
 import defaultImage from '../../assets/images/logo-blog-micro.png';
 
 export const GET: APIRoute = async (context) => {
-  // eslint-disable-next-line @typescript-eslint/no-magic-numbers
-  const ONE_WEEK_IN_MINUTES = 60 * 24 * 7;
-  // eslint-disable-next-line @typescript-eslint/no-magic-numbers
-  const TEN_KB_IN_BYTES = 10 * 1024;
+	// eslint-disable-next-line @typescript-eslint/no-magic-numbers
+	const ONE_WEEK_IN_MINUTES = 60 * 24 * 7;
+	// eslint-disable-next-line @typescript-eslint/no-magic-numbers
+	const TEN_KB_IN_BYTES = 10 * 1024;
 
-  const blogImage = await getImage({ src: defaultImage, format: 'png', width: 512, height: 512 });
+	const blogImage = await getImage({ src: defaultImage, format: 'png', width: 512, height: 512 });
 
-  return rss({
-    title: "Marco Campos' Blog",
-    description: 'A space where I talk about web development, Vue.js, Node.js, TypeScript, JavaScript',
-    site: context.site ?? '',
-    xmlns: { atom: 'http://www.w3.org/2005/Atom' },
-    stylesheet: '/feed.xsl',
-    customData: `
-      <language>en-us</language>
-      <atom:link href="${context.site?.toString() ?? ''}" rel="self" type="application/rss+xml" />
-      <image>
-        <url>${new URL(blogImage.src, context.site).toString()}</url>
-        <title>Marco Campos' Blog</title>
-        <link>${context.site?.toString() ?? ''}</link>
-        <width>144</width>
-        <height>144</height>
-      </image>
-      <lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
-      <ttl>${ONE_WEEK_IN_MINUTES}</ttl>
-    `,
-    items: await Promise.all((await listAllPosts()).map(async (post) => {
-      let image;
-      let imageSize = TEN_KB_IN_BYTES;
+	return rss({
+		title: "Marco Campos' Blog",
+		description: 'A space where I talk about web development, Vue.js, Node.js, TypeScript, JavaScript',
+		site: context.site ?? '',
+		xmlns: {
+			atom: 'http://www.w3.org/2005/Atom',
+			media: 'http://search.yahoo.com/mrss/'
+		},
+		stylesheet: '/feed.xsl',
+		customData: `
+			<language>en-us</language>
+			<atom:link href="${context.site?.toString() ?? ''}" rel="self" type="application/rss+xml" />
+			<image>
+				<url>${new URL(blogImage.src, context.site).toString()}</url>
+				<title>Marco Campos' Blog</title>
+				<link>${context.site?.toString() ?? ''}</link>
+				<width>144</width>
+				<height>144</height>
+			</image>
+			<lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
+			<ttl>${ONE_WEEK_IN_MINUTES}</ttl>
+		`,
+		items: await Promise.all((await listAllPosts()).map(async (post) => {
+			let image;
+			let imageSize = TEN_KB_IN_BYTES;
 
-      if (post.data.image) {
-        const sourceFilePath = post.data.image.src.replace(/^\/@fs/iu, '').replace(/\?.+$/iu, '');
-        const buildFilePath = import.meta.url.replace(/\/dist\/.*$/iu, `/dist${sourceFilePath}`).replace(/^file:\/\//iu, '');
+			if (post.data.image) {
+				const sourceFilePath = post.data.image.src.replace(/^\/@fs/iu, '').replace(/\?.+$/iu, '');
+				const buildFilePath = import.meta.url.replace(/\/dist\/.*$/iu, `/dist${sourceFilePath}`).replace(/^file:\/\//iu, '');
 
-        image = await getImage({ src: post.data.image, format: 'png' });
+				image = await getImage({ src: post.data.image, format: 'png' });
 
-        // If we are in dev mode, the paths will be diferent.
-        // If we are in build mode, one will be part of the other.
-        if (buildFilePath.includes(sourceFilePath)) {
-          imageSize = (await stat(buildFilePath)).size;
-        } else {
-          imageSize = (await stat(sourceFilePath)).size;
-        }
-      }
+				// If we are in dev mode, the paths will be diferent.
+				// If we are in build mode, one will be part of the other.
+				if (buildFilePath.includes(sourceFilePath)) {
+					imageSize = (await stat(buildFilePath)).size;
+				} else {
+					imageSize = (await stat(sourceFilePath)).size;
+				}
+			}
 
-      const item: RSSFeedItem = {
-        title: post.data.title,
-        description: post.data.summary,
-        pubDate: post.data.createdAt,
-        categories: post.data.tags,
-        link: `${context.site?.toString().replace(/\/$/u, '') ?? ''}/blog/${post.url}`,
-        ...(image && {
-          enclosure: {
-            url: new URL(image.src, context.site).toString(),
-            type: 'image/png',
-            length: imageSize
-          }
-        })
-      };
+			const item: RSSFeedItem = {
+				title: post.data.title,
+				description: post.data.summary,
+				pubDate: post.data.createdAt,
+				categories: post.data.tags,
+				link: `${context.site?.toString().replace(/\/$/u, '') ?? ''}/blog/${post.url}`,
+				...(image && {
+					enclosure: {
+						url: new URL(image.src, context.site).toString(),
+						type: 'image/png',
+						length: imageSize
+					},
+					customData: `
+						<media:content
+							url="${new URL(image.src, context.site).toString()}"
+							type="image/png"
+							medium="image"
+							height="${image.options.height ?? '512'}"
+							width="${image.options.width ?? '512'}"
+						/>
+						<media:description type="plain">${post.data.imageAlt ?? ''}</media:description>
+					`
+				})
+			};
 
-      return item;
-    }))
-  });
+			return item;
+		}))
+	});
 };
