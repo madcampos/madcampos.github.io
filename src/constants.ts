fe64e62ca6ce6a359f288b3fc3e84fb8bf829d61
madcampos <madcampos@outlook.com>
2024-01-24 01:56:22 -0500

    feat: update blog layouts

---
+++ src/constants.ts
@@ -0,0 +1,31 @@
+export const SITE_TITLE = 'Marco Campos';
+export const SITE_DESCRIPTION = 'Marco Campos - Senior Web Developer - Vue.js, Node.js, TypeScript, JavaScript';
+export const SITE_TAGS = ['Senior web developer', 'web developer', 'vue.js', 'vue', 'node.js', 'javascript', 'typescript', 'webdev'];
+
+export const LOGO_ALT = 'My moniker, "madcampos", on a monospaced font, in blue, between curly braces.';
+export const LOGO_MINI_ALT = 'The letters "madc" on a monospaced font, in blue, between curly braces.';
+export const LOGO_MICRO_ALT = 'The letter "m" on a monospaced font, in blue, between curly braces.';
+
+export const BLOG_TITLE = `Blog | ${SITE_TITLE}`;
+export const BLOG_DESCRIPTION = 'Marco Campos\' Blog - A space where I talk about web development, Vue.js, Node.js, TypeScript, JavaScript';
+export const BLOG_TAGS = ['blog', ...SITE_TAGS];
+
+export const BLOG_LOGO_ALT = 'Logo for madcampos\' blog consisting of my moniker, "madcampos", on a monospaced font, in blue, between curly braces. Below it, a subtext of "blog" in orange on the lower right corner.';
+export const BLOG_LOGO_MINI_ALT = 'Logo for madcampos\' blog consisting of the letters "madc" on a monospaced font, in blue, between curly braces. Below it, a subtext of "blog" in orange on the lower right corner.';
+export const BLOG_LOGO_MICRO_ALT = 'Logo for madcampos\' blog consisting of the letter "m" on a monospaced font, in blue, between curly braces. Below it, a subtext of "blog" in orange on the lower right corner.';
+
+export const PROJECTS_TITLE = `Projects | ${SITE_TITLE}`;
+export const PROJECTS_DESCRIPTION = 'Marco Campos\' Projects - A list of my projects';
+export const PROJECTS_TAGS = ['projects', ...SITE_TAGS];
+
+export const PROJECTS_LOGO_ALT = 'Logo for madcampos\' projects consisting of my moniker, "madcampos", on a monospaced font, in blue, between curly braces. Below it, a subtext of "projects" in orange on the lower right corner.';
+export const PROJECTS_LOGO_MINI_ALT = 'Logo for madcampos\' projects consisting of the letters "madc" on a monospaced font, in blue, between curly braces. Below it, a subtext of "projects" in orange on the lower right corner.';
+export const PROJECTS_LOGO_MICRO_ALT = 'Logo for madcampos\' projects consisting of the letter "m" on a monospaced font, in blue, between curly braces. Below it, a subtext of "projects" in orange on the lower right corner.';
+
+export const TALKS_TITLE = `Talks | ${SITE_TITLE}`;
+export const TALKS_DESCRIPTION = 'Marco Campos\' Talks - Talks I\'ve given at conferences and meetups.';
+export const TALKS_TAGS = ['talks', ...SITE_TAGS];
+
+export const TALKS_LOGO_ALT = 'Logo for madcampos\' talks consisting of my moniker, "madcampos", on a monospaced font, in blue, between curly braces. Below it, a subtext of "talks" in orange on the lower right corner.';
+export const TALKS_LOGO_MINI_ALT = 'Logo for madcampos\' talks consisting of the letters "madc" on a monospaced font, in blue, between curly braces. Below it, a subtext of "talks" in orange on the lower right corner.';
+export const TALKS_LOGO_MICRO_ALT = 'Logo for madcampos\' talks consisting of the letter "m" on a monospaced font, in blue, between curly braces. Below it, a subtext of "talks" in orange on the lower right corner.';
