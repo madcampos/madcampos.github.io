5148c3381a2d966166ac22fe68c16a9cc3755697
madcampos <madcampos@outlook.com>
2024-02-01 02:37:30 -0500

    fix: small tweak to how title is displayed along with extra metadata

--- src/constants.ts
+++ src/constants.ts
@@ -8,7 +8,7 @@ export const LOGO_MINI_ALT = 'The letters "madc" on a monospaced font, in blue, 
 export const LOGO_MICRO_ALT = 'The letter "m" on a monospaced font, in blue, between curly braces.';
 
 export const BLOG_URL = '/blog';
-export const BLOG_TITLE = `Blog | ${SITE_TITLE}`;
+export const BLOG_TITLE = ['Blog', SITE_TITLE];
 export const BLOG_DESCRIPTION = 'Marco Campos\' Blog - A space where I talk about web development, Vue.js, Node.js, TypeScript, JavaScript';
 export const BLOG_TAGS = ['blog', ...SITE_TAGS];
 export const BLOG_TAGS_URL = `${BLOG_URL}/tags`;
@@ -18,7 +18,7 @@ export const BLOG_LOGO_MINI_ALT = 'Logo for madcampos\' blog consisting of the l
 export const BLOG_LOGO_MICRO_ALT = 'Logo for madcampos\' blog consisting of the letter "m" on a monospaced font, in blue, between curly braces. Below it, a subtext of "blog" in orange on the lower right corner.';
 
 export const PROJECTS_URL = '/projects';
-export const PROJECTS_TITLE = `Projects | ${SITE_TITLE}`;
+export const PROJECTS_TITLE = ['Projects', SITE_TITLE];
 export const PROJECTS_DESCRIPTION = 'Marco Campos\' Projects - A list of my projects';
 export const PROJECTS_TAGS = ['projects', ...SITE_TAGS];
 
@@ -27,7 +27,7 @@ export const PROJECTS_LOGO_MINI_ALT = 'Logo for madcampos\' projects consisting 
 export const PROJECTS_LOGO_MICRO_ALT = 'Logo for madcampos\' projects consisting of the letter "m" on a monospaced font, in blue, between curly braces. Below it, a subtext of "projects" in orange on the lower right corner.';
 
 export const TALKS_URL = '/talks';
-export const TALKS_TITLE = `Talks | ${SITE_TITLE}`;
+export const TALKS_TITLE = ['Talks', SITE_TITLE];
 export const TALKS_DESCRIPTION = 'Marco Campos\' Talks - Talks I\'ve given at conferences and meetups.';
 export const TALKS_TAGS = ['talks', ...SITE_TAGS];
 
