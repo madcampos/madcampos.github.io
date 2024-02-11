d11b88e28440813bed331307e6129e432a7d5de0
madcampos <madcampos@outlook.com>
2024-02-11 16:11:35 -0500

    feat: update constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,8 @@
+// Site wide constants
 export const SITE_URL = '/';
-export const SITE_TITLE = 'Marco Campos';
-export const SITE_DESCRIPTION = 'Marco Campos - Senior Web Developer - Vue.js, Node.js, TypeScript, JavaScript';
+export const SITE_TITLE = ['Marco Campos'];
+export const SITE_TITLE_STRING = 'Marco Campos — Senior Web Developer';
+export const SITE_DESCRIPTION = 'Marco Campos — Senior Web Developer — Vue.js, Node.js, TypeScript, JavaScript';
 export const SITE_TAGS = [
 	'Senior web developer',
 	'web developer',
@@ -16,30 +18,52 @@ export const LOGO_ALT = 'My moniker, "madcampos", on a monospaced font, in blue,
 export const LOGO_MINI_ALT = 'The letters "madc" on a monospaced font, in blue, between curly braces.';
 export const LOGO_MICRO_ALT = 'The letter "m" on a monospaced font, in blue, between curly braces.';
 
+// "Error" constants
+export const ERROR_URL = '/404';
+export const ERROR_TITLE = ['404', ...SITE_TITLE];
+export const ERROR_TITLE_STRING = '404 — Not Found';
+export const ERROR_DESCRIPTION = '404 — Not Found';
+
+export const OFFLINE_URL = '/offline';
+export const OFFLINE_TITLE = ['Offline Page', ...SITE_TITLE];
+export const OFFLINE_TITLE_STRING = 'Offline Page';
+export const OFFLINE_DESCRIPTION = 'Marco Campos — Offline Page';
+
+// "Blog" constants
 export const BLOG_URL = '/blog';
-export const BLOG_TITLE = ['Blog', SITE_TITLE];
-export const BLOG_DESCRIPTION = 'Marco Campos\' Blog - A space where I talk about web development, Vue.js, Node.js, TypeScript, JavaScript';
+export const BLOG_TITLE_STRING = "Marco Campos' Blog";
+export const BLOG_TITLE = ['Blog', ...SITE_TITLE];
+export const BLOG_DESCRIPTION = "Marco Campos' Blog — A space where I talk about web development and other programming related (or not) things.";
 export const BLOG_TAGS = ['blog', ...SITE_TAGS];
+
+export const BLOG_LOGO_ALT = `Logo for madcampos' blog consisting of my moniker, "madcampos", on a monospaced font, in blue, between curly braces. Below it, a subtext of "blog" in orange on the lower right corner.`;
+export const BLOG_LOGO_MINI_ALT = `Logo for madcampos' blog consisting of the letters "madc" on a monospaced font, in blue, between curly braces. Below it, a subtext of "blog" in orange on the lower right corner.`;
+export const BLOG_LOGO_MICRO_ALT = `Logo for madcampos' blog consisting of the letter "m" on a monospaced font, in blue, between curly braces. Below it, a subtext of "blog" in orange on the lower right corner.`;
+
+// "Blog tags list" constants
 export const BLOG_TAGS_URL = `${BLOG_URL}/tags`;
+export const BLOG_TAGS_TITLE_STRING = "Tags for Marco Campos' blog";
+export const BLOG_TAGS_TITLE = ['Tags', ...BLOG_TITLE];
+export const BLOG_TAGS_DESCRIPTION = "Marco Campos' Blog — Tags for my blog posts";
 
-export const BLOG_LOGO_ALT = 'Logo for madcampos\' blog consisting of my moniker, "madcampos", on a monospaced font, in blue, between curly braces. Below it, a subtext of "blog" in orange on the lower right corner.';
-export const BLOG_LOGO_MINI_ALT = 'Logo for madcampos\' blog consisting of the letters "madc" on a monospaced font, in blue, between curly braces. Below it, a subtext of "blog" in orange on the lower right corner.';
-export const BLOG_LOGO_MICRO_ALT = 'Logo for madcampos\' blog consisting of the letter "m" on a monospaced font, in blue, between curly braces. Below it, a subtext of "blog" in orange on the lower right corner.';
-
+// "Projects" constants
 export const PROJECTS_URL = '/projects';
-export const PROJECTS_TITLE = ['Projects', SITE_TITLE];
-export const PROJECTS_DESCRIPTION = 'Marco Campos\' Projects - A list of my projects';
+export const PROJECTS_TITLE = ['Projects', ...SITE_TITLE];
+export const PROJECTS_TITLE_STRING = 'Projects';
+export const PROJECTS_DESCRIPTION = "Marco Campos' Projects — A list of projects I've worked on, or am currently working on. Mostly web development";
 export const PROJECTS_TAGS = ['projects', ...SITE_TAGS];
 
-export const PROJECTS_LOGO_ALT = 'Logo for madcampos\' projects consisting of my moniker, "madcampos", on a monospaced font, in blue, between curly braces. Below it, a subtext of "projects" in orange on the lower right corner.';
-export const PROJECTS_LOGO_MINI_ALT = 'Logo for madcampos\' projects consisting of the letters "madc" on a monospaced font, in blue, between curly braces. Below it, a subtext of "projects" in orange on the lower right corner.';
-export const PROJECTS_LOGO_MICRO_ALT = 'Logo for madcampos\' projects consisting of the letter "m" on a monospaced font, in blue, between curly braces. Below it, a subtext of "projects" in orange on the lower right corner.';
+export const PROJECTS_LOGO_ALT = `Logo for madcampos' projects consisting of my moniker, "madcampos", on a monospaced font, in blue, between curly braces. Below it, a subtext of "projects" in orange on the lower right corner.`;
+export const PROJECTS_LOGO_MINI_ALT = `Logo for madcampos' projects consisting of the letters "madc" on a monospaced font, in blue, between curly braces. Below it, a subtext of "projects" in orange on the lower right corner.`;
+export const PROJECTS_LOGO_MICRO_ALT = `Logo for madcampos' projects consisting of the letter "m" on a monospaced font, in blue, between curly braces. Below it, a subtext of "projects" in orange on the lower right corner.`;
 
+// "Talks" constants
 export const TALKS_URL = '/talks';
-export const TALKS_TITLE = ['Talks', SITE_TITLE];
-export const TALKS_DESCRIPTION = 'Marco Campos\' Talks - Talks I\'ve given at conferences and meetups.';
+export const TALKS_TITLE = ['Talks', ...SITE_TITLE];
+export const TALKS_TITLE_STRING = 'Talks, Presentations & Workshops';
+export const TALKS_DESCRIPTION = "Marco Campos' Talks — A list of talks I've given on meetups, conferences, or other events.";
 export const TALKS_TAGS = ['talks', ...SITE_TAGS];
 
-export const TALKS_LOGO_ALT = 'Logo for madcampos\' talks consisting of my moniker, "madcampos", on a monospaced font, in blue, between curly braces. Below it, a subtext of "talks" in orange on the lower right corner.';
-export const TALKS_LOGO_MINI_ALT = 'Logo for madcampos\' talks consisting of the letters "madc" on a monospaced font, in blue, between curly braces. Below it, a subtext of "talks" in orange on the lower right corner.';
-export const TALKS_LOGO_MICRO_ALT = 'Logo for madcampos\' talks consisting of the letter "m" on a monospaced font, in blue, between curly braces. Below it, a subtext of "talks" in orange on the lower right corner.';
+export const TALKS_LOGO_ALT = `Logo for madcampos' talks consisting of my moniker, "madcampos", on a monospaced font, in blue, between curly braces. Below it, a subtext of "talks" in orange on the lower right corner.`;
+export const TALKS_LOGO_MINI_ALT = `Logo for madcampos' talks consisting of the letters "madc" on a monospaced font, in blue, between curly braces. Below it, a subtext of "talks" in orange on the lower right corner.`;
+export const TALKS_LOGO_MICRO_ALT = `Logo for madcampos' talks consisting of the letter "m" on a monospaced font, in blue, between curly braces. Below it, a subtext of "talks" in orange on the lower right corner.`;
