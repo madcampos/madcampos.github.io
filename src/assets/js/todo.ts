916093f9bca5e5d44c1ed992f27790c1a9c53b98
madcampos <madcampos@outlook.com>
2024-05-29 02:53:30 -0400

    feat: update todo list

--- src/assets/js/todo.ts
+++ src/assets/js/todo.ts
@@ -1,3 +1,82 @@
 document.addEventListener('DOMContentLoaded', () => {
-	// TODO: implement local storage saving of TODO list
+	document.addEventListener('change', (evt) => {
+		const target = evt.target as HTMLInputElement;
+
+		if (target.matches('input[type="checkbox"]')) {
+			if (target.hasAttribute('checked')) {
+				evt.preventDefault();
+				evt.stopPropagation();
+
+				target.checked = true;
+
+				return;
+			}
+
+			localStorage.setItem(target.id, target.checked.toString());
+
+			const childrenInputs = [...target.closest('label')?.nextElementSibling?.querySelectorAll<HTMLInputElement>('input[type="checkbox"]') ?? []];
+
+			childrenInputs.forEach((childInput) => {
+				if (!childInput.ariaDisabled) {
+					childInput.checked = target.checked;
+				}
+			});
+
+			const checkedChildrenInputs = childrenInputs.filter(({ checked }) => checked).length;
+
+			if (checkedChildrenInputs > 0 && checkedChildrenInputs < childrenInputs.length) {
+				target.indeterminate = true;
+			} else {
+				target.indeterminate = false;
+			}
+
+			const parentInput = document.querySelector<HTMLInputElement>(`label:has( + ul #${target.id}) input`);
+
+			if (parentInput) {
+				const siblingInputs = [...target.closest('ul')?.querySelectorAll<HTMLInputElement>('input[type="checkbox"]') ?? []];
+				const checkedInputs = siblingInputs.filter(({ checked }) => checked).length;
+
+				parentInput.checked = checkedInputs === siblingInputs.length;
+
+				if (checkedInputs > 0 && checkedInputs < siblingInputs.length) {
+					parentInput.indeterminate = true;
+				} else {
+					parentInput.indeterminate = false;
+				}
+
+				localStorage.setItem(parentInput.id, parentInput.checked ? 'true' : 'false');
+			}
+		}
+	});
+
+	document.querySelectorAll<HTMLInputElement>('input[type="checkbox"]').forEach((input) => {
+		const isCurrentlyChecked = localStorage.getItem(input.id) === 'true';
+		let newChecked = false;
+
+		if (input.checked || isCurrentlyChecked) {
+			newChecked = true;
+		}
+
+		if (input.hasAttribute('checked')) {
+			input.ariaDisabled = 'true';
+		}
+
+		input.checked = newChecked;
+		localStorage.setItem(input.id, newChecked.toString());
+	});
+
+	document.querySelectorAll('label:has( + ul)').forEach((label) => {
+		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+		const input = label.querySelector<HTMLInputElement>('input[type="checkbox"]')!;
+		const childInputs = [...label.nextElementSibling?.querySelectorAll<HTMLInputElement>('input[type="checkbox"]') ?? []];
+		const checkedInputs = childInputs.filter(({ checked }) => checked).length;
+
+		if (checkedInputs === childInputs.length) {
+			input.checked = true;
+		}
+
+		if (checkedInputs > 0 && checkedInputs < childInputs.length) {
+			input.indeterminate = true;
+		}
+	});
 });
