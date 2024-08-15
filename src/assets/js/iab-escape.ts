3442fa6428812d764d88caae795060b10eec16cc
madcampos <madcampos@outlook.com>
2024-08-15 11:23:20 -0400

    add initial implementation for in app browser escape hatch

--- src/assets/js/iab-escape.ts
+++ src/assets/js/iab-escape.ts
@@ -1,18 +1,27 @@
 // Ref: https://frontendmasters.com/blog/the-pitfalls-of-in-app-browsers/
 
 import InAppSpy from 'inapp-spy';
+
+// eslint-disable-next-line new-cap
 const { isInApp } = InAppSpy();
 
 const url = `https://madcampos.dev`;
 const intentLink = `intent:${url}#Intent;end`;
+const shortcutsLink = `shortcuts://x-callback-url/run-shortcut?name=${crypto.randomUUID()}&x-error=${encodeURIComponent(url)}`;
 
 if (isInApp) {
-	window.location.replace(intentLink);
+	let link = shortcutsLink;
+
+	if (navigator.userAgent.includes('Android')) {
+		link = intentLink;
+	}
+
+	window.location.replace(link);
 
 	const $div = document.createElement('div');
 	$div.innerHTML = `
 		<p>Tap the button to open in your default browser</p>
-		<a href="${intentLink}" target="_blank">Open</a>
+		<a href="${link}" target="_blank">Open</a>
 	`;
 	document.body.insertAdjacentElement('afterbegin', $div);
 }
