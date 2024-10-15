a118ff90dbd27a7713eb16f46c2b9b1f063a3d3c
madcampos <madcampos@outlook.com>
2024-10-15 15:07:06 -0400

    feat: add comments on path regexes

--- src/utils/path.ts
+++ src/utils/path.ts
@@ -12,16 +12,23 @@ export function join(paths: string[], options?: Partial<JoinOptions>) {
 		...options
 	};
 
-	const joinedPath = paths.join(opts.separator).replaceAll(new RegExp(`(?<!:)${opts.separator}{2,}`, 'giu'), opts.separator);
+	// Matches two or more slashes, except when following the ":" character.
+	// This is so the two slashes on "https://" and similar are not replaced.
+	const doubleslashesRegex = `(?<!:)${opts.separator}{2,}`;
+
+	// Matches one or more slashes at the end of the string.
+	const trailingSlashesRegex = `${opts.separator}+$`;
+
+	const joinedPath = paths.join(opts.separator).replaceAll(new RegExp(doubleslashesRegex, 'giu'), opts.separator);
 
 	let normalizedPath = joinedPath;
 
 	switch (opts.trailingSlash) {
 		case true:
-			normalizedPath = `${joinedPath}${opts.separator}`.replaceAll(new RegExp(`(?<!:)${opts.separator}{2,}`, 'giu'), opts.separator);
+			normalizedPath = `${joinedPath}${opts.separator}`.replace(new RegExp(trailingSlashesRegex, 'iu'), opts.separator);
 			break;
 		case false:
-			normalizedPath = joinedPath.replace(new RegExp(`${opts.separator}$`, 'iu'), '');
+			normalizedPath = joinedPath.replace(new RegExp(trailingSlashesRegex, 'iu'), '');
 			break;
 		default:
 	}
