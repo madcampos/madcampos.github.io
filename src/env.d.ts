3a0389bd601d407602d0c7c2448f67ed9e40d100
madcampos <me@madcampos.dev>
2024-11-22 23:17:11 -0500

    fix: update weird issue with astro env config

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,5 @@
 /// <reference path="../.astro/types.d.ts" />
-/// <reference types="astro/client" />
+/// <reference path="../.astro/icon.d.ts" />
 
 /// <reference types="vite-plugin-pwa/client" />
 /// <reference types="vite-plugin-pwa/info" />
