ef13ec113fc5d730d74e951c33b5ddf2376e2861
madcampos <me@madcampos.dev>
2024-12-10 15:57:08 -0500

    feat: update settings logic to be a self contained class

--- src/assets/js/settings.ts
+++ src/assets/js/settings.ts
@@ -1,15 +1,83 @@
-interface SiteSettings {
-	theme?: string;
-}
+export class SiteSettings {
+	static #searchParams?: URLSearchParams;
 
-document.addEventListener('DOMContentLoaded', () => {
-	const settings: SiteSettings = {
-		theme: localStorage.getItem('theme') ?? undefined
-	};
+	static #initializeSettings() {
+		if (SiteSettings.#searchParams) {
+			return;
+		}
 
-	for (const [setting, value] of Object.entries(settings)) {
-		if (value !== undefined) {
-			document.documentElement.dataset[setting] = value;
+		SiteSettings.#searchParams = new URLSearchParams(document.location.search);
+
+		const settings = Object.getOwnPropertyNames(SiteSettings).filter((key) => !['name', 'length', 'prototype'].includes(key)) as (keyof typeof SiteSettings)[];
+		for (const setting of settings) {
+			SiteSettings.#updateSetting(setting, SiteSettings[setting]?.toString());
 		}
 	}
-});
+
+	static #getSetting(setting: string) {
+		SiteSettings.#initializeSettings();
+
+		return SiteSettings.#searchParams?.get(setting) ?? localStorage.getItem('setting') ?? undefined;
+	}
+
+	static #updateSetting(setting: string, value: string | undefined) {
+		SiteSettings.#initializeSettings();
+
+		// eslint-disable-next-line @typescript-eslint/no-dynamic-delete
+		delete document.documentElement.dataset[setting];
+		SiteSettings.#searchParams?.delete(setting);
+		localStorage.removeItem(setting);
+
+		if (value) {
+			document.documentElement.dataset[setting] = value;
+			SiteSettings.#searchParams?.set(setting, value);
+
+			if (SiteSettings.shouldSaveSettings) {
+				localStorage.setItem(setting, value);
+			}
+		}
+
+		const shouldShareSettings = SiteSettings.debug || SiteSettings.shouldShareSettings;
+		const hasSearchParams = SiteSettings.#searchParams !== undefined && SiteSettings.#searchParams.size > 0;
+
+		if (shouldShareSettings && hasSearchParams) {
+			document.location.search = SiteSettings.#searchParams?.toString() ?? '';
+		}
+	}
+
+	static get shouldSaveSettings() {
+		return SiteSettings.#getSetting('saveSettings') === 'true';
+	}
+
+	static get shouldShareSettings() {
+		return SiteSettings.#getSetting('shareSettings') === 'true';
+	}
+
+	static set shouldShareSettings(value: boolean) {
+		SiteSettings.#updateSetting('shareSettings', value ? 'true' : 'false');
+	}
+
+	static get debug() {
+		return SiteSettings.#getSetting('debug') === 'true';
+	}
+
+	static set debug(value: boolean) {
+		SiteSettings.#updateSetting('debug', value ? 'true' : 'false');
+	}
+
+	static get theme() {
+		return SiteSettings.#getSetting('theme');
+	}
+
+	static set theme(value: string | undefined) {
+		SiteSettings.#updateSetting('theme', value);
+	}
+
+	static get pwa() {
+		return SiteSettings.#getSetting('pwa');
+	}
+
+	static set pwa(value: string | undefined) {
+		SiteSettings.#updateSetting('pwa', value);
+	}
+}
