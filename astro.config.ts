57c5cbb012238649862ee303918be43ef35fe7c0
madcampos <me@madcampos.dev>
2024-12-18 00:27:35 -0500

    fix: add an actual ignore to cursom theme shennanigans

--- astro.config.ts
+++ astro.config.ts
@@ -61,7 +61,7 @@ export default defineConfig({
 			themes: {
 				light: 'light-plus',
 				dark: 'dark-plus',
-				// @ts-expect-error - Astro is fussy about custom theme
+				// @ts-ignore - Astro is fussy about custom theme
 				contrast: hcShikiTheme
 			},
 			defaultColor: false,
